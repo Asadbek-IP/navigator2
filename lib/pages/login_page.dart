7dd0ee50840266ea3c8f5f8e9269c7d375f408a7
asadbek-ip <dasturchi6678@gmail.com>
2022-11-10 15:43:20 +0500

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:navigator/pages/home_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String? ism = "Yulduz";
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>  HomePage(name: "Marjona",),
+              ),
+            );
+          },
+          child: const Text("Knopka"),
+        ),
+      ),
+    );
+  }
+}
